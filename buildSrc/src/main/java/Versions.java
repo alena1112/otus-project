5e62a57bd2faac9c7209aecc0f6568be3a4e29f2
alena.kovalenko <alena.kovalenko@lamoda.ru>
2022-03-19 11:36:21 +0300

    HW_09 JDBC

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -2,6 +2,6 @@ public interface Versions {
     String guava = "31.0.1-jre";
     String commonsCollections = "4.4";
     String jol = "0.16";
-    String glassfishJson = "1.1.4";
-    String protobuf = "3.19.4";
+
+    String testcontainers = "1.15.3";
 }
